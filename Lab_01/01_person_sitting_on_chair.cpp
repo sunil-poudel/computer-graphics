921cb82270001629e0e2a4f853ca6880d91a4cdf
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-21 23:45:30 -0700

    one step previous commit

--- Lab_01/01_person_sitting_on_chair.cpp
+++ Lab_01/01_person_sitting_on_chair.cpp
@@ -3,18 +3,13 @@
 
 int main(){
 	int gd = DETECT, gm;
-	initgraph(&gd, &gm, NULL);\
-	setbkcolor(WHITE);
-	cleardevice();
+	initgraph(&gd, &gm, NULL);
 	
-	setcolor(BLACK);
-	setfillstyle(SOLID_FILL, BLUE);
 	// Chair
 	line(10, 200, 10, 300);
 	line(10, 200, 150, 200);
 	line(150, 10, 150, 300);
 	
-	setcolor(BROWN);
 	//	man
 	circle(75, 70, 30);
 	line(75, 100, 75, 150);
@@ -23,14 +18,13 @@ int main(){
 	line(75, 105, 45, 150);
 	line(75, 105, 105, 150);
 	
-	setcolor(BLACK);
 	//eyes
 	circle(65, 65, 5);
 	circle(85, 65, 5);
 	circle(65, 65, 1);
 	circle(85, 65, 1);
 
-	setcolor(BROWN);
+	
 	//nose
 	circle(75, 80, 1);
 	
@@ -40,5 +34,3 @@ int main(){
 	getch();
     closegraph();
 }
-
-floodfill(x, y, BLUE);
