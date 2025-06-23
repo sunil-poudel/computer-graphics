97016aabc0f20e0c385943533d4ae55ec4423197
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-23 07:29:24 -0700

    ensure x1, y1 < x2 y2

--- Lab_02/Q2_BDA.cpp
+++ Lab_02/Q2_BDA.cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 void bda(int x1, int y1, int x2, int  y2){
+	if(x1>x2){
+		int temp = x1;
+		x1 = x2; 
+		x2 = temp;
+	}
+	if(y1>y2){
+		int temp = y1;
+		y1 = y2; 
+		y2 = temp;
+	}
+	
 	int maxHeight = getmaxy();
 	int maxWidth = getmaxx();
 	
