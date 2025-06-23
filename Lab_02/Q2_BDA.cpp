14ead22a8a22101ea7b820ab49f38bf228ce44fc
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-23 06:47:49 -0700

    draw x axis

--- Lab_02/Q2_BDA.cpp
+++ Lab_02/Q2_BDA.cpp
@@ -4,8 +4,20 @@
 using namespace std;
 
 void bda(int x1, int y1, int x2, int  y2){
+	int maxHeight = getmaxy();
+	int maxWidth = getmaxx();
 	
+	int centerHeight = maxHeight/2;
+	int centerWidth = maxWidth/2;
+	
+	line(0, centerHeight, maxWidth, centerHeight);
 }
 int main(){
+	int gd = DETECT, gm;
+	initgraph(&gd, &gm, NULL);
+	
+	bda(0,0,100,100);
+	getch();
+	closegraph();
 	
 }
