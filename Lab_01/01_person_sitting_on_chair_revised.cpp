d6564bc8bc1f00ff1314fb21be50e0cd936019d6
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-28 07:46:11 -0700

    draw legs and backrest

--- Lab_01/01_person_sitting_on_chair_revised.cpp
+++ Lab_01/01_person_sitting_on_chair_revised.cpp
@@ -8,7 +8,18 @@ void drawChairWithMan() {
     line(300, 300, 280, 270);     // right edge
     line(180, 270, 280, 270);     // back edge
 
-    
+    // Chair legs (verticals)
+    line(200, 300, 200, 350);     // front-left
+    line(300, 300, 300, 350);     // front-right
+    line(180, 270, 180, 320);     // back-left
+    line(280, 270, 280, 320);     // back-right
+
+    // Chair backrest (upright rectangle)
+    line(180, 270, 180, 200);
+    line(280, 270, 280, 200);
+    line(180, 200, 280, 200);
+
+   
 }
 
 int main() {
