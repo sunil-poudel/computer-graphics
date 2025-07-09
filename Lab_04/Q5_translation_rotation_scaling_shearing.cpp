0a5d55804ea2270830fac23c1c54ca2cf50475e2
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-08 18:26:11 -0700

    draw triangle based on input

--- Lab_04/Q5_translation_rotation_scaling_shearing.cpp
+++ Lab_04/Q5_translation_rotation_scaling_shearing.cpp
@@ -2,6 +2,12 @@
 #include <graphics.h>
 using namespace std;
 
+void drawTriangle(int x1, int x2, int x3, int y1, int y2, int y3){
+	line(x1, y1, x2, y2);
+	line(x1, y1, x3, y3);
+	line(x2, y2, x3, y3);
+}
+
 int main(){
 	int gd = DETECT , gm;
 	initgraph(&gd, &gm, "");
@@ -16,6 +22,7 @@ int main(){
 	cout<<"(x3,y3) ";
 	cin>>x3>>x3;
 	
+	drawTriangle(x1,x2,x3,y1,y2,y3);
 	
 	getch();
 	closegraph();
