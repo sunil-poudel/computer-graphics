4586520080652ed1ae166714830d210d6b1f62fa
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-08 18:57:50 -0700

    fit drawn triangle to axis successfully

--- Lab_04/Q5_translation_rotation_scaling_shearing.cpp
+++ Lab_04/Q5_translation_rotation_scaling_shearing.cpp
@@ -8,19 +8,24 @@ void drawTriangle(int x1, int x2, int x3, int y1, int y2, int y3){
 	line(x2, y2, x3, y3);
 }
 
-void drawAxes(){
+void drawAxes(int* x1, int* x2, int* x3, int* y1, int* y2, int* y3){
 	int maxHeight = getmaxy();
 	int maxWidth = getmaxx();
 	int centerX = maxWidth/2;
 	int centerY = maxHeight/2;
 	line(0, centerY, maxWidth, centerY);
 	line(centerX,0, centerX, maxHeight);
+	*x1+=centerX;
+	*x2+=centerX;
+	*x3+=centerX;
+	*y1=centerY-*y1;
+	*y2=centerY-*y2;
+	*y3=centerY-*y3;
 }
 
 int main(){
 	int gd = DETECT , gm;
 	initgraph(&gd, &gm, "");
-	drawAxes();
 	
 	int x1,x2,x3,y1,y2,y3;
 	
@@ -30,8 +35,10 @@ int main(){
 	cout<<"(x2,y2) ";
 	cin>>x2>>y2;	
 	cout<<"(x3,y3) ";
-	cin>>x3>>x3;
+	cin>>x3>>y3;
 	
+	
+	drawAxes(&x1,&x2,&x3,&y1,&y2,&y3);
 	drawTriangle(x1,x2,x3,y1,y2,y3);
 	
 	getch();
