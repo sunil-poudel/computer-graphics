c4c3e774450d898f9aa841fb6fdab2c49ace0e61
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-08 18:22:18 -0700

    input coordinates for triangle

--- Lab_04/Q5_translation_rotation_scaling_shearing.cpp
+++ Lab_04/Q5_translation_rotation_scaling_shearing.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <graphics.h>
+using namespace std;
 
 int main(){
 	int gd = DETECT , gm;
 	initgraph(&gd, &gm, "");
+	
+	int x1,x2,x3,y1,y2,y3;
+	
+	cout<<"input the coordinates of a triangle: "<<endl;
+	cout<<"(x1,y1) ";
+	cin>>x1>>y1;
+	cout<<"(x2,y2) ";
+	cin>>x2>>y2;	
+	cout<<"(x3,y3) ";
+	cin>>x3>>x3;
+	
 	
 	getch();
 	closegraph();
